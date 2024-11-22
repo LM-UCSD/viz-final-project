8b27dc30637061391c5a5ae46fc7186d041eed56
LM-UCSD <163374052+LM-UCSD@users.noreply.github.com>
2024-11-22 11:01:29 -0700

    Update global.js

--- global.js
+++ global.js
@@ -9,10 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
     navBar.innerHTML = `
         <ul class="navbar-list">
             <li class="navbar-item"><a href="${isHomePage ? "" : "../"}" class="navbar-link">Home</a></li>
-            <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/barchart/map.html" : "../barchart/map.html"}" class="navbar-link">Bar Chart</a></li>
-            <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/dotmap/dotmap.html" : "../dotmap/dotmap.html"}" class="navbar-link">Dot Chart</a></li>
-            <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/heatmap/heatmap.html" : "../heatmap/heatmap.html"}" class="navbar-link">Heat Map</a></li>
-            <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/bubble/bubble.html" : "../bubble/bubble.html"}" class="navbar-link">Bubble Map</a></li>
+            <li class="navbar-item"><a href="${isHomePage ? "barchart/map.html" : "../barchart/map.html"}" class="navbar-link">Bar Chart</a></li>
+            <li class="navbar-item"><a href="${isHomePage ? "dotmap/dotmap.html" : "../dotmap/dotmap.html"}" class="navbar-link">Dot Chart</a></li>
+            <li class="navbar-item"><a href="${isHomePage ? "heatmap/heatmap.html" : "../heatmap/heatmap.html"}" class="navbar-link">Heat Map</a></li>
+            <li class="navbar-item"><a href="${isHomePage ? "bubble/bubble.html" : "../bubble/bubble.html"}" class="navbar-link">Bubble Map</a></li>
         </ul>
     `;
     document.body.insertBefore(navBar, document.body.firstChild);
