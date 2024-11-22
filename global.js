8895d84445b41e86ce6342977f71b32e83db49de
LM-UCSD <163374052+LM-UCSD@users.noreply.github.com>
2024-11-22 10:54:20 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const navBar = document.createElement("nav");
     navBar.className = "navbar";
 
-    const isHomePage = window.location.pathname.endsWith("index.html");
+    const isHomePage = document.documentElement.classList.contains('home');
 
     navBar.innerHTML = `
         <ul class="navbar-list">
-            <li class="navbar-item"><a href="${isHomePage ? "" : "../"}index.html" class="navbar-link">Home</a></li>
+            <li class="navbar-item"><a href="${isHomePage ? "" : "../"}" class="navbar-link">Home</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/barchart/map.html" : "../barchart/map.html"}" class="navbar-link">Bar Chart</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/dotmap/dotmap.html" : "../dotmap/dotmap.html"}" class="navbar-link">Dot Chart</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "viz-final-project/heatmap/heatmap.html" : "../heatmap/heatmap.html"}" class="navbar-link">Heat Map</a></li>
