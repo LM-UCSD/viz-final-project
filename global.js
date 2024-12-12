1db55dbfa6b9ecf04bb53469778bc312f1866f37
Ann Meek <111289809+ameek12@users.noreply.github.com>
2024-12-12 06:50:12 -0800

    Update global.js
    Took out dot chart

--- global.js
+++ global.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
         <ul class="navbar-list">
             <li class="navbar-item"><a href="${isHomePage ? "" : "../"}" class="navbar-link">Home</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "barchart/map.html" : "../barchart/map.html"}" class="navbar-link">Bar Chart</a></li>
-            <li class="navbar-item"><a href="${isHomePage ? "dotmap/dotmap.html" : "../dotmap/dotmap.html"}" class="navbar-link">Dot Chart</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "heatmap/heatmap.html" : "../heatmap/heatmap.html"}" class="navbar-link">Heat Map</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "bubble/bubble.html" : "../bubble/bubble.html"}" class="navbar-link">Bubble Map</a></li>
             <li class="navbar-item"><a href="${isHomePage ? "animated_bar/index.html" : "../animated_bar/index.html"}" class="navbar-link">Animated Bar</a></li>
